66028b841ac25a7d7030a0c4956250ead42ed080
Jagui5 <101363377+Jagui5@users.noreply.github.com>
2025-08-29 11:22:53 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-3_UuzHZuQ53', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '71hr3acp2vjacrje3lirleo7p6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
