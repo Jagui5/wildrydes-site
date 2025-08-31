8dfe2ee174f0e10a12534f7bcd5a92cb44654903
Jagui5 <101363377+Jagui5@users.noreply.github.com>
2025-08-31 20:24:09 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-3_HSjLZnTLu', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5b4nhnf72rtf7lfjs900mgdtar', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-3_UuzHZuQ53', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '71hr3acp2vjacrje3lirleo7p6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://cls0n1ctt6.execute-api.eu-west-3.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
